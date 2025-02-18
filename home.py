ee3d0f99eceb7e5ea2f758ff7704337f8bf393dc
Surface <surface@gmail.com>
2025-02-18 12:19:10 -0800

    Add data to my API and made some changes

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask("Weather API")
 
@@ -8,7 +9,9 @@ def home():
 
 @app.route('/api/v1/<station>/<date>')
 def api(station, date):
-    weather = 12
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    weather = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {
         "Station": station,
         "date": date,
